3b19ac98cd61acfc337a8f40aaa319c390b25304
Aaron Quint <aaron@quirkey.com>
2012-11-03 23:03:15 -0400

    significant cleanup and refactor

--- lib/dayone.rb
+++ lib/dayone.rb
@@ -1,124 +1,44 @@
 class DayOne < Slogger
+
   def to_dayone(options = {})
     @dayonepath = storage_path
-    markdown = @dayonepath =~ /Journal[._]dayone\/?$/ ? false : true
     content = options['content'] || ''
-    unless markdown
-      uuid = options['uuid'] || %x{uuidgen}.gsub(/-/,'').strip
-      datestamp = options['datestamp'] || options['date'] || Time.now.utc.iso8601
-      entry = CGI.escapeHTML(content) unless content.nil?
-    else
-      img_path = false
-      uuid = options['uuid'] || false
-      if uuid
-        for ext in %w[jpg jpeg gif tiff svg png]
-          img_path = "../photos/#{uuid}.#{ext}" if File.exists?(@dayonepath+"/photos/#{uuid}.#{ext}")
-        end
-      end
-      entry = content.nil? ? '' : content
-      if img_path
-        entry = "![](#{img_path})\n\n" + entry
-      end
-      uuid = Time.now.strftime('%Y-%m-%d_%I%M%S')+"_"+(rand(5000).to_s)
-      if date = options['datestamp'] || options['date']
-        datestamp = Date.parse(date).strftime('%x')
-      else
-        datestamp = Time.now.strftime('%x')
-      end
-      puts datestamp.inspect
-    end
+    uuid = options['uuid']
+    datestamp = options['datestamp'] || options['date'] || Time.now.utc.iso8601
+    entry = CGI.escapeHTML(content) unless content.nil?
     starred = options['starred'] || false
 
-    # entry = CGI.escapeHTML(content.unpack('C*').pack('U*').gsub(/[^[:punct:]\w\s]+/,' ')) unless content.nil?
-
-    # @dayonepath = storage_path
     @log.info("=====[ Saving entry to entries/#{uuid} ]")
-    ext = markdown ? ".md" : ".doentry"
+    ext = ".doentry"
     entry_dir = File.join(File.expand_path(@dayonepath), "entries")
     Dir.mkdir(entry_dir, 0700) unless File.directory?(entry_dir)
-    fh = File.new("#{entry_dir}/#{uuid}#{ext}",'w+')
-    fh.puts @template.result(binding)
-    fh.close
+    File.open("#{entry_dir}/#{uuid}#{ext}",'w+') do |f|
+      f << @template.result(binding)
+    end
     return true
   end
 
-  def save_image(imageurl,uuid)
+  def save_image(imageurl, uuid)
     @dayonepath = Slogger.new.storage_path
     source = imageurl.gsub(/^https/,'http')
     match = source.match(/(\..{3,4})($|\?|%22)/)
     ext = match.nil? ? match[1] : '.jpg'
     @log.info("Original image has extension #{ext}. Coverting for Day One recognition.")
-    target = File.join(File.expand_path(@dayonepath),"photos","#{uuid}.jpg")
-    FileUtils.mkdir_p(File.dirname(target))
-    begin
-      Net::HTTP.get_response(URI.parse(imageurl)) do |http|
-        data = http.body
-        @log.info("Retrieving image -\n           Source: #{imageurl}\n      Target UUID: #{uuid}")
-        if data == false || data == 'false'
-          @log.warn("Download failed")
-          return false
-        else
-          File.open( File.expand_path(target), "wb" ) { |file| file.write(data) }
-        end
-      end
-      return target
-    #rescue Exception => e
-    #  p e
-    #  raise e
-    end
-  end
-
-  def process_image(image)
-    orig = File.expand_path(image)
-
-    match = orig.match(/(\..{3,4})$/)
-    return false if match.nil?
-    ext = match[1]
-    @log.info("Resizing image #{File.basename(orig)}")
-    res = %x{sips -Z 800 "#{orig}" 2>&1}
-    unless ext =~ /\.jpg$/
-      case ext
-      when '.jpeg'
-        target = orig.gsub(/\.jpeg$/,'.jpg')
-        FileUtils.mv(orig,target)
-        return target
-      # when /\.(png|gif|tiff)$/
-      #   if File.exists?('/usr/local/bin/convert')
-      #     target = orig.gsub(/#{ext}$/,'.jpg')
-      #     @log.info("Converting #{orig} to JPEG")
-      #     %x{/usr/local/bin/convert "#{orig}" -background white -mosaic +matte "#{target}"}
-      #     File.delete(orig)
-      #     return target
-      #   else
-      #     @log.warn("Image could not be converted to JPEG format and may not show up in Day One. Please install ImageMagick (available through brew).")
-      #     return orig
-      #   end
-      #   return orig
-      else
-        return orig
-      end
-    end
-    return orig
-  end
-
-  def store_single_photo(file, options = {}, copy = false)
-    options['content'] ||= File.basename(file,'.jpg') if @config['image_filename_is_title']
-    options['uuid'] ||= %x{uuidgen}.gsub(/-/,'').strip
-    options['starred'] ||= false
-    options['datestamp'] ||= Time.now.utc.iso8601
     photo_dir = File.join(File.expand_path(Slogger.new.storage_path), "photos")
-    Dir.mkdir(photo_dir, 0700) unless File.directory?(photo_dir)
-
-    target_path = File.join(photo_dir,options['uuid']+".jpg")
-
-    if copy
-      FileUtils.copy(File.expand_path(file),target_path)
-      file = target_path
+    FileUtils..mkdir_p(photo_dir) unless File.directory?(photo_dir)
+    target = File.join(photo_dir,"#{uuid}.jpg")
+    Net::HTTP.get_response(URI.parse(imageurl)) do |http|
+      data = http.body
+      @log.info("Retrieving image -\n           Source: #{imageurl}\n      Target UUID: #{uuid}")
+      if data == false || data == 'false'
+        @log.warn("Download failed")
+        return false
+      else
+        puts "writing to #{target}"
+        File.open( File.expand_path(target), "wb" ) { |file| file.write(data) }
+      end
     end
-
-    res = self.process_image(File.expand_path(file))
-    if res
-      return self.to_dayone(options)
-    end
+    target
   end
+
 end
--- plugins/twitterlogger.rb
+++ plugins/twitterlogger.rb
@@ -24,134 +24,93 @@ config = {
 $slog.register_plugin({ 'class' => 'TwitterLogger', 'config' => config })
 
 require 'rexml/document'
+require 'digest/sha1'
 
 class TwitterLogger < Slogger
 
   def get_body(target, depth = 0)
-
     final_url = RedirectFollower.new(target).resolve
     url = URI.parse(final_url.url)
 
     host, port = url.host, url.port if url.host && url.port
     req = Net::HTTP::Get.new(url.path)
     res = Net::HTTP.start(host, port) {|http| http.request(req) }
-
-    return res.body
+    res.body
   end
 
   def download_images(images)
-
     images.each do |image|
-      options = {}
-      options['content'] = image['content']
-      options['uuid'] = %x{uuidgen}.gsub(/-/,'').strip
+      image['uuid'] = generate_uuid_for_tweet(image['content'])
       sl = DayOne.new
-      path = sl.save_image(image['url'],options['uuid'])
-      sl.store_single_photo(path,options) unless path == false
+      path = sl.save_image(image['url'], image['uuid'])
+      sl.to_dayone(image)
     end
-
-    return true
+    true
   end
 
-  def get_tweets(user,type='timeline')
+  def get_tweets(user, type='timeline', max_id = nil)
     @log.info("Getting Twitter #{type} for #{user}")
     if type == 'favorites'
-      url = URI.parse("http://api.twitter.com/1/favorites.xml?count=200&screen_name=#{user}&include_entities=true&count=200")
+      url = "http://api.twitter.com/1/favorites.xml?count=200&screen_name=#{user}&include_entities=true&count=200"
     else
-      url = URI.parse("http://api.twitter.com/1/statuses/user_timeline.xml?screen_name=#{user}&count=200&exclude_replies=true&include_entities=true")
+      url = "http://api.twitter.com/1/statuses/user_timeline.xml?screen_name=#{user}&count=200&exclude_replies=true&include_entities=true"
+      url << "&max_id=#{max_id}" if max_id
     end
     tweets = []
     images = []
     begin
       begin
-        res = Net::HTTP.get_response(url).body
+        res = Net::HTTP.get_response(URI.parse(url)).body
       rescue Exception => e
         @log.warn("Failure getting response from Twitter")
-        # p e
         return false
       end
-      REXML::Document.new(res).elements.each("statuses/status") { |tweet|
-        today = @timespan
+      oldest_id = nil
+      REXML::Document.new(res).elements.each("statuses/status") do |tweet|
         tweet_date = Time.parse(tweet.elements['created_at'].text)
-        @log.warn(tweet_date)
-        # break if tweet_date < today
         tweet_text = tweet.elements['text'].text.gsub(/\n/,"\n\t")
         if type == 'favorites'
-          # TODO: Prepend favorite's username/link
           screen_name = tweet.elements['user/screen_name'].text
-          tweet_text = "[#{screen_name}](http://twitter.com/#{screen_name}): #{tweet_text}"
+          tweet_text = "*favorite* [#{screen_name}](http://twitter.com/#{screen_name}): #{tweet_text}"
         end
         tweet_id = tweet.elements['id'].text
         unless tweet.elements['entities/urls'].nil? || tweet.elements['entities/urls'].length == 0
-          tweet.elements.each("entities/urls/url") { |url|
+          tweet.elements.each("entities/urls/url") do |url|
             tweet_text.gsub!(/#{url.elements['url'].text}/,"[#{url.elements['display_url'].text}](#{url.elements['expanded_url'].text})")
-          }
+          end
         end
-        begin
-          if @twitter_config['save_images']
-            tweet_images = []
-            unless tweet.elements['entities/media'].nil? || tweet.elements['entities/media'].length == 0
-              tweet.elements.each("entities/media/creative") { |img|
-                tweet_images << { 'content' => tweet_text, 'date' => tweet_date.utc.iso8601, 'url' => img.elements['media_url'].text }
+        if @twitter_config['save_images']
+          tweet_images = []
+          unless tweet.elements['entities/media'].nil? || tweet.elements['entities/media'].length == 0
+            tweet.elements.each("entities/media/creative") do |img|
+              tweet_images << {
+                'content' => tweet_text,
+                'datestamp' => tweet_date.utc.iso8601,
+                'url' => img.elements['media_url'].text
               }
             end
-
-              # new logic for the picture links and added yfrog (nr)
-            tweet_text.scan(/\((http:\/\/twitpic.com\/\w+?)\)/).each do |picurl|
-              aurl=URI.parse(picurl[0])
-              burl="http://twitpic.com/show/large#{aurl.path}"
-              curl = RedirectFollower.new(burl).resolve
-              final_url=curl.url
-              tweet_images << { 'content' => tweet_text, 'date' => tweet_date.utc.iso8601, 'url' => final_url } unless final_url.nil?
-              #tweet_images=[tweet_text,tweet_date.utc.iso8601,final_url] unless final_url.nil?
-            end
-            tweet_text.scan(/\((http:\/\/campl.us\/\w+?)\)/).each do |picurl|
-              aurl=URI.parse(picurl[0])
-              burl="http://campl.us/#{aurl.path}:800px"
-              curl = RedirectFollower.new(burl).resolve
-              final_url=curl.url
-              tweet_images << { 'content' => tweet_text, 'date' => tweet_date.utc.iso8601, 'url' => final_url } unless final_url.nil?
-            end
-            # Drop.lr downloads temporarily broken
-            # tweet_text.scan(/\((http:\/\/#{@twitter_config['droplr_domain']}\/\w+?)\)/).each do |picurl|
-            #   # final_url = self.get_body(picurl[0]).match(/"(https:\/\/s3.amazonaws.com\/files.droplr.com\/.+?)"/)
-            #   tweet_images << { 'content' => tweet_text, 'date' => tweet_date.utc.iso8601, 'url' => picurl[0]+"+" } # unless final_url.nil?
-            # end
-            tweet_text.scan(/\((http:\/\/instagr\.am\/\w\/\w+?\/)\)/).each do |picurl|
-              final_url=self.get_body(picurl[0]).match(/http:\/\/distilleryimage.....[a-z]+.com[\W][a-z0-9_]+.jpg/)
-              tweet_images << { 'content' => tweet_text, 'date' => tweet_date.utc.iso8601, 'url' => final_url[0] } unless final_url.nil?
-            end
-            tweet_text.scan(/http:\/\/[\w\.]*yfrog\.com\/[\w]+/).each do |picurl|
-              aurl=URI.parse(picurl)
-              burl="http://yfrog.com#{aurl.path}:medium"
-              curl = RedirectFollower.new(burl).resolve
-              final_url=curl.url
-              tweet_images << { 'content' => tweet_text, 'date' => tweet_date.utc.iso8601, 'url' => final_url } unless final_url.nil?
-            end
           end
-        rescue Exception => e
-          @log.warn("Failure gathering image urls")
-          p e
+          tweet_images.concat parse_images_from_tweet(tweet_text, tweet_date)
         end
         if tweet_images.empty?
-          tweets.push("* [[#{tweet_date.strftime('%I:%M %p')}](https://twitter.com/#{user}/status/#{tweet_id})] #{tweet_text}")
+          tweets.push({
+            'content' => "* [[#{tweet_date.strftime('%I:%M %p')}](https://twitter.com/#{user}/status/#{tweet_id})] #{tweet_text}",
+            'uuid' => generate_uuid_for_tweet(tweet_text),
+            'datestamp' => tweet_date.utc.iso8601
+          })
         else
           images.concat(tweet_images)
         end
-      }
-      if @twitter_config['save_images'] && images != []
-        #begin
-          self.download_images(images)
-        # rescue Exception => e
-        #  @log.warn("Failure downloading images")
-          # p e
-        #end
+        oldest_id = tweet.elements['id_str'].text
       end
-      return tweets.reverse.join("\n")
-    #rescue Exception => e
-    #  @log.warn("Error getting #{type} for #{user}")
-      # p e
-    #  return false
+      if @twitter_config['save_images'] && images != []
+        self.download_images(images)
+      end
+      if tweets.length > 0
+        older_tweets = get_tweets(user, type, oldest_id)
+        tweets << older_tweets
+      end
+      tweets
     end
 
   end
@@ -175,11 +134,12 @@ class TwitterLogger < Slogger
     @twitter_config['twitter_tags'] ||= ''
     tags = "\n\n#{@twitter_config['twitter_tags']}\n" unless @twitter_config['twitter_tags'] == ''
 
+    tweets = []
     @twitter_config['twitter_users'].each do |user|
       retries = 0
       success = false
       until success
-        tweets = self.get_tweets(user,'timeline')
+        tweets.concat get_tweets(user,'timeline')
         if tweets
           success = true
         else
@@ -192,8 +152,9 @@ class TwitterLogger < Slogger
       retries = 0
       success = false
       until success
-        favs = self.get_tweets(user,'favorites')
+        favs = get_tweets(user,'favorites')
         if favs
+          tweets.concat favs
           success = true
         else
           break if $options[:max_retries] == retries
@@ -202,15 +163,45 @@ class TwitterLogger < Slogger
           sleep 2
         end
       end
-      unless tweets == ''
-        tweets = "## Tweets\n\n### Posts by @#{user} on #{Time.now.strftime('%m-%d-%Y')}\n\n#{tweets}#{tags}"
-        sl.to_dayone({'content' => tweets})
-      end
-      unless favs == ''
-        favs = "## Favorite Tweets\n\n### Favorites from @#{user} for #{Time.now.strftime('%m-%d-%Y')}\n\n#{favs}#{tags}"
-        sl.to_dayone({'content' => favs})
+      tweets.each do |tweet|
+        sl.to_dayone(tweet)
       end
     end
+  end
+
+  def parse_images_from_tweet(tweet_text, tweet_date)
+    tweet_images = []
+    urls = []
+    tweet_text.scan(/\((http:\/\/twitpic.com\/\w+?)\)/).each do |picurl|
+      aurl=URI.parse(picurl[0])
+      burl="http://twitpic.com/show/large#{aurl.path}"
+      curl = RedirectFollower.new(burl).resolve
+      urls << curl.url
+    end
+    tweet_text.scan(/\((http:\/\/campl.us\/\w+?)\)/).each do |picurl|
+      aurl=URI.parse(picurl[0])
+      burl="http://campl.us/#{aurl.path}:800px"
+      curl = RedirectFollower.new(burl).resolve
+      urls << curl.url
+    end
+    tweet_text.scan(/\((http:\/\/instagr\.am\/\w\/\w+?\/)\)/).each do |picurl|
+      final_url = self.get_body(picurl[0]).match(/http:\/\/distilleryimage.....[a-z]+.com[\W][a-z0-9_]+.jpg/)
+      urls << final_url[0] if final_url
+    end
+    tweet_text.scan(/http:\/\/[\w\.]*yfrog\.com\/[\w]+/).each do |picurl|
+      aurl=URI.parse(picurl)
+      burl="http://yfrog.com#{aurl.path}:medium"
+      curl = RedirectFollower.new(burl).resolve
+      urls << curl.url
+    end
+    urls.compact.each do |url|
+      tweet_images << { 'content' => tweet_text, 'datestamp' => tweet_date.utc.iso8601, 'url' => url}
+    end
+    tweet_images
+  end
+
+  def generate_uuid_for_tweet(text)
+    Digest::SHA1.hexdigest(text)[0...32].upcase
   end
 
 end
